33023fe0cf21e5adcd2ef61a08781d1c9fea779f
MatthewSamaha <62967648+mjsamaha@users.noreply.github.com>
2025-03-01 15:55:50 -0500

    Update ttt.c

--- ttt.c
+++ ttt.c
@@ -25,3 +25,24 @@ void printReferenceBoard(){
     printf("---+---+---\n");
     printf(" 7 | 8 | 9 \n\n");
 }
+
+/* printing the board in a 3x3 format -- using % to add | */
+void printBoard() {
+    printf("\nCurrent Board:\n");
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ') {
+            printf(" %d ", i + 1);  // Show available position number
+        } else {
+            printf(" %c ", board[i]);  // Show 'X' or 'O'
+        }
+        
+        if ((i + 1) % 3 == 0)
+            printf("\n");
+        else
+            printf("|");
+        
+        if (i == 2 || i == 5)
+            printf("---+---+---\n");
+    }
+    printf("\n");
+}
