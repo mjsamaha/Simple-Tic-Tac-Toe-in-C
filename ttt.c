9486ec1d7b98f824dcd91f0e0ed2d83ecba59d78
MatthewSamaha <62967648+mjsamaha@users.noreply.github.com>
2025-03-01 13:36:56 -0500

    Update ttt.c
    setting board and printing our reference board

--- ttt.c
+++ ttt.c
@@ -8,3 +8,20 @@
 #define EMPTY ' ' 
 
 char board[9]; 
+
+/* setting the board all empty at the start */
+void initBoard(){
+	for (int i = 0; i < 9; i++){
+		board[i] = EMPTY; // fill all elements with empty spaces
+	}
+}
+
+/* print tic tac toe board with coord for the user */
+void printReferenceBoard(){
+	printf("\nReference Board:\n");
+    printf(" 1 | 2 | 3 \n");
+    printf("---+---+---\n");
+    printf(" 4 | 5 | 6 \n");
+    printf("---+---+---\n");
+    printf(" 7 | 8 | 9 \n\n");
+}
