e94ca50d65710dcc8bc3610376bff7bc4c330092
MatthewSamaha <62967648+mjsamaha@users.noreply.github.com>
2025-03-01 12:47:54 -0500

    Create ttt.c

---
+++ ttt.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <stdlib.h> // for the game to make random moves
+#include <time.h> // seed random num generator
+
+/* defining constants */
+#define PLAYER 'X' // user symbol
+#define COMPUTER 'O' // computer's symbol
+#define EMPTY ' ' // defines empty space on board
+
+char board[9]; 
